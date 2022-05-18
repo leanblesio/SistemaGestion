5aebccc4c2b654a4429a2d9c73c8b2f30ce59779
leanblesio <leanblesio@gmail.com>
2022-05-18 19:32:45 -0300

    Entidades y HTML de pag

--- js/main.js
+++ js/main.js
@@ -5,6 +5,55 @@ const Proveedores = []; //array de objetos con proveedores
 const Productos = []; //array de objetos con productos
 const Stocks = []; //array de objetos con stocks
 
+
+//Generar un mnetodo del objeto ARRAY que compruebe si existe  ese valor
+Array.prototype.Existe = function(x){
+ let mapping = this.map(x=> x.nombre) //Solo para comparar nombres array de string
+ return new Set(mapping).has(x) //hay dentro de esta lista el valor x?
+
+}
+
+class CRUD{
+
+        constructor(colleccionTipo){ //Si es cliente, pedido, proveedor, producto, stocks.
+            this.colleccionTipo = colleccionTipo.toLowerCase();
+            switch(colleccionTipo.toUpperCase()){
+                case "CLIENTES":
+                    this.array = Clientes;
+                    break;
+                
+                case "PEDIDOS":
+                    this.array = Pedidos;
+                
+                case "PROVEEDORES":
+                    this.array = Proveedores;
+                    break;
+                    
+                case "PRODUCTOS":
+                    this.array = Productos;
+                    break;
+                 
+                case "STOCKS":
+                    this.array = Stocks;    
+                    
+            }
+        }
+
+        Create(objetoTipo){
+            if(this.array.Existe(objetoTipo)){
+                console.log('YA EXISTE')
+            }
+
+        }
+
+}
+
+
+let crudClientes = new CRUD("CLIENTES"); 
+crudClientes.Create(new Clientes());//INGRESAR FUNCION PARA QUE TOME LOS DATOS
+
+
+
 //ENTIDAD CLIENTE
 class Cliente{
     constructor(codigo, nombre, direccion, zona, horario, telefono){
--- js/producto.js
+++
@@ -1,8 +0,0 @@
-class Producto{
-    constructor(codigo, descripcion, precio, stock){
-        this.codigo = codigo;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.stock = stock;
-    }
-}
